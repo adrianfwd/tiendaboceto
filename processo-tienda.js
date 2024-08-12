5bc950c48923ea7b5e7d2685dbc08ad1e3193753
adrianfwd <azuniga@fwdcostarica.com>
2024-08-12 09:29:58 -0600

    backup

--- processo-tienda.js
+++ processo-tienda.js
@@ -1,42 +1,42 @@
 var articulos = [];
 
 function crearArticulo() {
-  var nombre = document.getElementById("articuloNombre").value;
-  var precio = document.getElementById('articuloPrecio').value;
-  var descripcion = document.getElementById('articuloDescripcion').value;
+  var nombre = document.getElementById("nombre").value;
+  var precio = document.getElementById('precio').value;
+  var descripcion = document.getElementById('descripcion').value;
 
   if (nombre && precio && descripcion) {
     var articulo = {
       nombre: nombre,
-      precio: parseFloat(precio),
+      precio: precio,
       descripcion: descripcion
     };
     articulos.push(articulo);
     localStorage.setItem('articulos', JSON.stringify(articulos));
-   
+
   }
-  mostrarArticulos()
+  mostrarArticulos();
 }
 
 
 function mostrarArticulos() {
   var listaDiv = document.getElementById('listaArticulos');
-  listaDiv.innerHTML = '';
-
-
   var articulosGuardados = localStorage.getItem('articulos');
+
   if (articulosGuardados) {
     articulos = JSON.parse(articulosGuardados);
   }
-  
+
   for (var i = 0; i < articulos.length; i++) {
     var articulo = articulos[i];
-    var articuloDiv = document.createElement('div');
-    articuloDiv.innerHTML = 'Nombre:' + articulo.nombre + '<br>' +
-      '<strong>Precio:</strong> $' + articulo.precio.toFixed(2) + '<br>' +
-      '<strong>Descripción:</strong> ' + articulo.descripcion + '<br><br>';
-    listaDiv.appendChild(articuloDiv);
+
+    let dArticulo = document.createElement("p")
+    dArticulo.textContent = articulo.descripcion;
+
   }
+
+  var articuloDiv = document.createElement('div');
+  articuloDiv.appendChild(dArticulo);
 
 }
 
